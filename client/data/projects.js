3832dce1d60f326b84251309f44b31fbf1ed0b88
Keeley Nakamoto <keeleynakamoto@gmail.com>
2016-10-27 12:20:42 -0500

    (feat) Add dummy project data

---
+++ client/data/projects.js
@@ -0,0 +1,46 @@
+var projects = [
+  {
+    id: 'followthru',
+    position: '',
+    name: 'followthru',
+    subtitle: 'FRONT-END ARCHITECTURE - API INTEGRATIONS - UI',
+    type: 'mobile',
+    image1: 'followthru1.png',
+    image2: 'https://d3bfbpfngihhgs.cloudfront.net/followthru2.png',
+    link: 'https://vimeo.com/173844724',
+    button: 'VIEW DEMO',
+    technologies: 'React Native, Redux, Node, Express, and PostgreSQL',
+    description: 'Followthru is an iPhone productivity application for quickly creating smart lists. These lists help users follow through on completing added items by providing easy access to those items and next steps to complete the item through multiple API interactions. I led the team to build this from idea to implementation.'
+  },
+  {
+    id: 'savor',
+    position: '',
+    name: 'Savor',
+    subtitle: 'BACK-END STRUCTURE - AUTHENTICATION - DEPLOYMENT - UI',
+    type: 'web',
+    image1: 'savor1.png',
+    image2: 'https://d3bfbpfngihhgs.cloudfront.net/savor2.png',
+    link: 'https://savor-app.herokuapp.com/#/',
+    button: 'VIEW SITE',
+    technologies: 'Angular, Node, Express, and MongoDB',
+    description: 'Savor was developed in order to better record and share meals amongst friends. I engineered a server/database overhaul to go to a user-focused system. In addition to refocusing the UI, I worked to ensure the application was deployed and properly handled user authentication.'
+  },
+  {
+    id: 'trex-racer',
+    position: '',
+    name: 'T-Rex Racer',
+    subtitle: 'BACK-END DESIGN - UI - FRONT-END FUNCTIONALITY',
+    type: 'web',
+    image1: 'trex-racer1.png',
+    image2: 'https://d3bfbpfngihhgs.cloudfront.net/trex-racer2.png',
+    link: 'https://trex-timer.herokuapp.com/#/signin',
+    button: 'VIEW SITE',
+    technologies: 'Angular, Node, Express, Socket.IO, and MongoDB',
+    description: 'Ever find yourself using a boring stopwatch? T-Rex Racer is an interactive timer that has a fun multiplayer chat and game functionality. I worked across the stack and not only implemented front-end game functionality but also designed the back-end to track game statistics and persist user information.'
+  }
+];
+
+export default projects;
+
+  }
+]
