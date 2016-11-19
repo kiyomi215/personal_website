fafe284c0dfdbf118d6de7887855702381a06221
Keeley Nakamoto <keeleynakamoto@gmail.com>
2016-11-18 23:26:28 -0600

    (fix) Restructure project skeleton

--- server/server.js
+++ server/server.js
@@ -2,3 +2,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
+var port = process.eng.PORT || 8080;
+
+app.use(express.static(__dirname + '/client'));
