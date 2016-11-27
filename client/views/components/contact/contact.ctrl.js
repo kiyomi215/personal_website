628137df22268373dbf17949855b8c5c98b9a852
Keeley Nakamoto <keeleynakamoto@gmail.com>
2016-11-26 23:52:51 -0800

    (feat) Being nodemailer setup on contact ctrl

--- client/views/components/contact/contact.ctrl.js
+++ client/views/components/contact/contact.ctrl.js
@@ -2,4 +2,16 @@
 
 angular.module('personalWebsite.contact', [])
   .controller('contactController', ['$scope', function($scope) {
+    var email = {
+      sender: 'Keeley Nakamoto',
+      recipient: $scope.contact.name,
+      phone: $scope.contact.number,
+      email: $scope.contact.email,
+      message: $scope.contact.message,
+      sendCopy: true
+    }
+    function sendEmail() {
+
+    }
+
   }]);
