479ed49bdf0eafd1ae8148e8792a58d9c851f571
Keeley Nakamoto <keeleynakamoto@gmail.com>
2016-11-27 22:47:55 -0800

    (feat) Begin post request for contact form

--- client/views/components/contact/contact.ctrl.js
+++ client/views/components/contact/contact.ctrl.js
@@ -1,17 +1,25 @@
 "use strict";
 
 angular.module('personalWebsite.contact', [])
-  .controller('contactController', ['$scope', function($scope) {
-    var email = {
-      sender: 'Keeley Nakamoto',
-      recipient: $scope.contact.name,
-      phone: $scope.contact.number,
-      email: $scope.contact.email,
-      message: $scope.contact.message,
-      sendCopy: true
-    }
-    function sendEmail() {
+  .controller('contactController', ['$scope', '$http', function($scope, $http) {
+    function sendEmail(email) {
 
-    }
+      var email = {
+        sender: 'Keeley Nakamoto',
+        recipient: $scope.contact.name,
+        phone: $scope.contact.number,
+        email: $scope.contact.email,
+        message: $scope.contact.message,
+        sendCopy: true
+      }
+
+      $http.post('/contact-form', data)
+        .success(function(data, status, headers, config) {
+          console.log(data);
+        })
+        .error(function(data, status, headers, config) {
+          console.log('Error: ', data);
+        })
+    };
 
   }]);
