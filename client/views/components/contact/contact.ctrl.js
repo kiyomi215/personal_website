78bacae2e0bcbfdf83303f0008e67f168ee95071
Keeley Nakamoto <keeleynakamoto@gmail.com>
2016-12-04 23:25:07 -0600

    (feat) Refactor sendEmail function to replace deprecated .success and .error and remove uneneccesary parameters

--- client/views/components/contact/contact.ctrl.js
+++ client/views/components/contact/contact.ctrl.js
@@ -4,19 +4,18 @@ angular.module('personalWebsite.contact', [])
   .controller('contactController', ['$scope', '$http', function($scope, $http) {
     function sendEmail(email) {
       var email = {
-        sender: 'Keeley Nakamoto',
-        recipient: $scope.contact.name,
+        name: $scope.contact.name,
         phone: $scope.contact.number,
         email: $scope.contact.email,
         message: $scope.contact.message,
         sendCopy: true
-      }
+      };
 
       $http.post('/contact-form', email)
-        .success(function(email, status, headers, config) {
-          console.log(email);
+        .then(function(email, status) {
+          console.log('Success! 'email);
         })
-        .error(function(email, status, headers, config) {
+        .catch(function(email, status) {
           console.log('Error: ', email);
         })
     };
