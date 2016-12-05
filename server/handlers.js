5d82aa8a5924eaf75cfab0709c3d6e6e08c2e053
Keeley Nakamoto <keeleynakamoto@gmail.com>
2016-12-05 00:22:03 -0600

    (feat) Add conditional email recipient if client requests a copy of inquiry

--- server/handlers.js
+++ server/handlers.js
@@ -6,17 +6,22 @@ var transporter = nodemailer.createTransport();
 
 exports.sendEmail = function(req, res) {
   var email = req.body;
+  if(email.sendCopy) {
+    var recipient = '"Keeley Nakamoto" <keeleynakamoto@gmail.com>', email.contact.name + ' <'email.contact.email'>';
+  } else {
+    var recipient = '"Keeley Nakamoto" <keeleynakamoto@gmail.com>';
+  }
 
   transporter.sendMail({
     from: email.contact.email,
-    to: 'keeleynakamoto@gmail.com',
+    to: recipient,
     subject: 'Message from ' + email.contact.name,
     text: email.contact.number + ' ' + email.contact.message
-  }, function(err, data) {
+  }, function(err, info) {
     if(err) {
       console.log(err);
     } else {
-      console.log('Message sent: ', data.response);
+      console.log('Message sent: ', info.response);
     }
   });
   res.json(email);
