4a20de2ecaa36fc215439470769454e970d0dfe8
Keeley Nakamoto <keeleynakamoto@gmail.com>
2016-12-11 21:09:36 -0600

    (fix) Fix typo for process.env

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
-var port = process.eng.PORT || 8080;
+var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/client'));
 
